7d5033d04a0fa5269fb8f9793ba29d07b4fad4fd
Locomexicano68 <58477453+Locomexicano68@users.noreply.github.com>
2020-01-31 14:03:06 +0100

    Update processes.cpp

--- processes.cpp
+++ processes.cpp
@@ -1,3 +1,10 @@
+/*
+ * Berkant Ciftci
+ * 0979368
+ * TI1D
+ * Opdracht: process
+ */
+
 #include "tinbes.h"
 
 #define TABLE_SIZE 25
@@ -11,155 +18,202 @@ long addr[TABLE_SIZE];
 int noOfProcesses = 0;
 int idCounter = 0;
 
-int newProcess(string name, long address) {
-  // TO DO
-  if(name.size() <= 10)
+int newProcess(string name, long address)
+{
+
+  char sToChArray[name.length()];
+
+  if (noOfProcesses < 25)
   {
-    addr[idCounter] = address;
-    state[idCounter] = RUNNING;
-    id[idCounter] = idCounter;
-    processName[idCounter] = name;
-  
-  noOfProcesses++;
-  return idCounter++;
-  } 
-  else {
+    if (name.length() > 10)
+    {
+      cout << "Het maximale aantal karakters in de naam is overschreden!" << endl;
+      return -1;
+    }
+    else
+    {
+      for (int i = 0; i < sizeof(sToCh); i++)
+      {
+        sToChArray[i] = name[i];
+        if (sToCh[i] == '0')
+        {
+          cout << " 0 is een verboden karakter!" << endl;
+          return -1;
+        }
+        for (int i = 0; i < noOfProcesses; i++)
+        {
+          if (processName[i].compare(name) == 0)
+          {
+            cout << "Name is already taken." << endl;
+            return -1;
+          }
+        }
+        processName[noOfProcesses] = name;
+        id[noOfProcesses] = idCounter;
+        addr[noOfProcesses] = address;
+        state[noOfProcesses] = PAUSED;
+      }
+    }
+  }
+  else
+  {
+    cout << "Het maximale aantal processen is overschreden!" << endl;
     return -1;
   }
-  // DONE
+
+  noOfProcesses++;
+  return idCounter++;
 }
 
-void removeProcess(int i) {
+void removeProcess(int i)
+{
   noOfProcesses--;
-  for (int j = i; j < noOfProcesses; j++) 
+  for (int j = i; j < noOfProcesses; j++)
   {
-    // TO DO
-    id[i]= (id[i+1])-1;
-    state[i]=state[i+1];
-    processName[i]= processName[i+1];
-    addr[i]= (addr[i+1])-1;
 
-    // DONE
-
+    processName[j] = processName[j + 1];
+    id[j] = id[j + 1];
+    addr[j] = addr[j + 1];
+    state[j] = state[j + 1];
   }
 }
 
-void executeProcesses() {
+void executeProcesses()
+{
   long newAddr;
-  for (int i = 0; i < noOfProcesses; i++) {
-    if (state[i] == RUNNING) {
-      // TO DO
-        newAddr = addr[i]--;
-      // DONE
-      if (newAddr == 0) {
+  for (int i = 0; i < noOfProcesses; i++)
+  {
+    if (state[i] == RUNNING)
+    {
+
+      newAddr = addr[i] - 1;
+      if (newAddr == 0)
+      {
         cout << "Process \"" << processName[i] << "\" has terminated." << endl;
         removeProcess(i--);
-      } else {
+      }
+      else
+      {
+        cout << processName[i] << ' ' << addr[i] << endl;
         addr[i] = newAddr;
       }
     }
   }
 }
 
-void listProcesses() {
-  for (int i = 0; i < noOfProcesses; i++) {
+void listProcesses()
+{
+  for (int i = 0; i < noOfProcesses; i++)
+  {
     cout << id[i] << ' ' << processName[i] << ' ' << state[i] << endl;
   }
 }
 
-int findProcess(int i) {
-  for (int j = 0; j < noOfProcesses; j++) 
+int findProcess(int i)
+{
+  for (int j = 0; j < noOfProcesses; j++)
   {
-    // TO DO
-      if( j == i){
-        return j;
-      }
-    // DONE
 
-    
-     
+    if (id[j] == i)
+    {
+      return j;
+    }
   }
   return -1;
 }
 
-void suspendProcess(int i) {
+void suspendProcess(int i)
+{
   int j = findProcess(i);
-  if (j == -1) {
+  if (j == -1)
+  {
     cout << "Process does not exist." << endl;
     return;
   }
-  if (state[j] == PAUSED) {
+  if (state[j] == PAUSED)
+  {
     cout << "Process already paused." << endl;
   }
-  else {
-    // TO DO
-    state[j] = PAUSED;
-    // DONE
+  else
+  {
 
+    state[j] = PAUSED;
+    cout << "Process \"" << processName[j] << "\" has been suspended." << endl;
   }
 }
 
-void resumeProcess(int i) {
+void resumeProcess(int i)
+{
   int j = findProcess(i);
-  if (j == -1) {
+  if (j == -1)
+  {
     cout << "Process does not exist." << endl;
     return;
   }
-  if (state[j] == RUNNING) {
+  if (state[j] == RUNNING)
+  {
     cout << "Process already running." << endl;
   }
-  else {
-    // TO DO
+  else
+  {
+
     state[j] = RUNNING;
-    // DONE
-
+    cout << "Process \"" << processName[j] << "\" has been resumed." << endl;
   }
 }
 
-void killProcess(int i) {
+void killProcess(int i)
+{
   int j = findProcess(i);
-  if (j == -1) {
+  if (j == -1)
+  {
     cout << "Process does not exist." << endl;
-  } else {
-    // TO DO
-    processName[j] = -1;
-    id[j] = -1;
-    state[j] = -1;
-    addr[j] = -1;
-    noOfProcesses--;
-    // DONE
-    
+  }
+  else
+  {
+
+    cout << "Process \"" << processName[j] << "\" has been removed." << endl;
+    removeProcess(j);
   }
 }
 
-int main() {
+int main()
+{
   unblockCin();
   string command;
-  while (true) {
-    if (charactersAvailable()) {
+  while (true)
+  {
+    if (charactersAvailable())
+    {
       command = readLine();
-      if (command == "RUN") {
+      if (command == "RUN")
+      {
         string name = readLine();
         long address = stoi(readLine());
         cout << newProcess(name, address) << endl;
       }
-      if (command == "LIST") {
+      if (command == "LIST")
+      {
         listProcesses();
       }
-      if (command == "SUSPEND") {
+      if (command == "SUSPEND")
+      {
         int i = stoi(readLine());
         suspendProcess(i);
       }
-      if (command == "RESUME") {
+      if (command == "RESUME")
+      {
         int i = stoi(readLine());
         resumeProcess(i);
       }
-      if (command == "KILL") {
+      if (command == "KILL")
+      {
         int i = stoi(readLine());
         killProcess(i);
       }
     }
-    else {
+    else
+    {
       executeProcesses();
     }
   }
